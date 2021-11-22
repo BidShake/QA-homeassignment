7ebe24d62634660797bd7ebf158646bcc7dac50b
ThomasBUEE <>
2021-11-22 10:08:30 +0000

    Deploying to gh-pages from @ BidShake/QA-homeassignment@eeea611451911cbac3d9f5871e0eeed00b522d72 🚀

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.933b1186e8cfe980a504f4aaa1c96838.js"
+  "/QA-homeassignment/precache-manifest.7c094f6ae33ede41b4a5a4853631d511.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/QA-homeassignment/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
